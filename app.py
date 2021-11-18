6f8972f6cab806bafd3020d2f4d77b30bb93ba12
Thamas21 <53061030+Thamas21@users.noreply.github.com>
2021-11-18 13:54:51 -0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,9 @@
+import models
+
+# main menu, add, search, analysis, exit, view
+# add_book function
+# edit function
+#  delete_books function
+# search_books
+# data_clean
+# loop that runs program 
---
+++ models.py
@@ -0,0 +1,6 @@
+# create database
+# books.db
+# create model
+# title, author, date_published, price
+
+
