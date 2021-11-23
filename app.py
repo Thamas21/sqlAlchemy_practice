1890c8f5baf50f4ba76f7eb8b480e6a72d723105
Thamas21 <53061030+Thamas21@users.noreply.github.com>
2021-11-23 11:24:40 -0800

    app.py update
    This commit adds a rough outline of the main logical functions.

--- app.py
+++ app.py
@@ -1,12 +1,50 @@
+import sys
 from models import (Base, session,
                     Book, engine)
+
+
+def menu():
+    while True:
+        print('''
+            \nPROMANNING BOOK
+            \r1) Add a book
+            \r2) View all books
+            \r3) Search for a book
+            \r4) Book Analysis
+            \r5) Exit
+            ''')
+        input('What do you want to do? ')
+
+def add():
+    pass
+
+def view_all():
+    pass
+
+def search():
+    pass
+
+def analysis():
+    pass
+
+
+def exit(input):
+    if input == 5:
+        sys.exit()
+
+
+
+
 
 # main menu, add, search, analysis, exit, view
 # add_book function
 # edit function
-#  delete_books function
+# delete_books function
 # search_books
 # data_clean
 # loop that runs program
+
+
 if __name__ == "__main__":
     Base.metadata.create_all(engine)
+    menu()
--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import (create_engine, Column, String, 
+from sqlalchemy import (create_engine, Column, String,
                         Integer, Date)
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
