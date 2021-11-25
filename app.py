8ce507a824766300e7814a3eb2735152ff2b8ce9
Thamas21 <53061030+Thamas21@users.noreply.github.com>
2021-11-24 16:58:33 -0800

    app.py updates
    This commit adds the ability to edit and delete a book from the database.

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import time
 # search, analysis,
 # edit function
 # delete_books function
-# search_books
 
 
 def menu():
@@ -29,6 +28,23 @@ def menu():
                 \rPress enter to continue. ''')
 
 
+def sub_menu():
+    while True:
+        print('''
+            \n1) Edit
+            \r2) Delete
+            \r3) Return to main menu''')
+        choice = input('What do you want to do? ')
+        if choice in ['1', '2', '3']:
+            return choice
+
+        else:
+            input('''
+                \rPlease choose one of the options above.
+                \rA number from 1-3.
+                \rPress enter to continue. ''')
+
+
 def clean_date(date_str):
     months = ['January', 'February', 'March', 'April', 'May', 'June',
               'July', 'August', 'September', 'October', 'November', 'December']
@@ -41,7 +57,7 @@ def clean_date(date_str):
         return_date = datetime.date(year, month, day)
     except ValueError:
         input('''
-            \n***  DATE ERROR *** 
+            \n***  DATE ERROR ***
             \rThe date format should include a valid Month, Date, Year from the past
             \rEx: Januarly 13, 2003
             \rPress enter to try again
@@ -57,7 +73,7 @@ def clean_price(price_str):
 
     except ValueError:
         input('''
-            \n***  Price Error *** 
+            \n***  Price Error ***
             \rThe price format should include a number without a currency symbol
             \rEx: 10.99
             \rPress enter to try again
@@ -86,6 +102,30 @@ def clean_id(id_string, options):
             \rPress enter to try again
             \r***************** ''')
             return
+
+
+def edit_check(column_name, current_value):
+    print(f'\n**** EDIT {column_name} *****')
+    if column_name == 'Price':
+        print(f"\r Current Value: {current_value/100}")
+    elif column_name == 'Date':
+        print(f'\rCurrnet Value: {current_value.strftime("%B %d, %Y")}')
+    else:
+        print(f'\rCurrent Value: {current_value}')
+
+    if column_name == 'Date' or column_name == 'Price':
+        while True:
+            changes = input('What would you like to change the value to? ')
+            if column_name == 'Date':
+                changes = clean_date(changes)
+                if type(changes) == datetime.date:
+                    return changes
+            elif column_name == 'Price':
+                changes = clean_price(changes)
+                if type(changes) == int:
+                    return changes
+    else:
+        return input('What would you like to change the value to? ')
 
 
 def add_csv():
@@ -135,7 +175,7 @@ def app():
                         \rTitle: {book.title}
                         \rAuthor: {book.author}
                         ''')
-            input('Press enter to return to the main menu')
+            input('Press enter to return to the main menu. ')
         elif choice == '3':
             id_options = []
             for book in session.query(Book):
@@ -154,7 +194,22 @@ def app():
                 \rPublished: {the_book.date_published}
                 \rPrice: ${the_book.price / 100}
             ''')
-            input('Press enter to return to the main menu. ')
+            sub_choice = sub_menu()
+            if sub_choice == '1':
+                # Edit functinality
+                the_book.title = edit_check('Title', the_book.title)
+                the_book.author = edit_check('Author', the_book.author)
+                the_book.date_published = edit_check('Date', the_book.date_published)
+                the_book.price = edit_check('Price', the_book.price)
+                session.commit()
+                print('Book updated!')
+                time.sleep(1.5)
+            elif sub_choice == '2':
+                session.delete(the_book)
+                session.commit()
+                print('Book deleted')
+                time.sleep(1.5)
+
         elif choice == '4':
             # book analysis
             pass
@@ -168,5 +223,5 @@ if __name__ == "__main__":
 
     add_csv()
     app()
-    for book in session.query(Book):
-        print(book)
+    # for book in session.query(Book):
+    #     print(book)
